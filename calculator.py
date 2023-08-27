27802dc5baafa6e1c7b051524c3a644c01f74299
chandru200613 <143263456+chandru200613@users.noreply.github.com>
2023-08-27 12:10:06 +0530

    create calculator.py

---
+++ calculator.py
@@ -0,0 +1,22 @@
+print("muthulakshmi please enter the value")
+print("1.add")
+print("2.sub")
+print("3.mul")
+print("4.div")
+num1=int(input("enter a num1 :"))
+num2=int(input("enter a num2 :"))
+print("1.add")
+print("2.sub")
+print("3.mul")
+print("4.div")
+ch=int(input("enter ur chioce:"))
+if ch==1:
+    print(num1+num2)
+elif ch==2:
+    print(num1-num2)
+elif ch==3:
+    print(num1*num2)
+elif ch==4:
+    print(num1/num2)
+else :
+    print ("u have entered the worng value5")
